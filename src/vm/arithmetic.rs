8200a0c36eccc5d0e24449e7ed5cff959f8ed815
duckie451 <96787824+duckie451@users.noreply.github.com>
2022-01-18 23:49:39 +0530

    Moved helper functions from `common.rs` to `arithmetic.rs`

--- src/vm/arithmetic.rs
+++ src/vm/arithmetic.rs
@@ -1,6 +1,41 @@
 use crate::error::{raise_internal, Data};
-use crate::vm::common::*;
 use crate::vm::opcode::{OpCode, Value};
+
+pub fn get_type(value: &Value) -> String {
+    match value {
+        Value::String(_) => "string".to_string(),
+        Value::Integer(_) => "integer".to_string(),
+        Value::Float(_) => "float".to_string(),
+        //Value::List(_) => "list".to_string(),
+        Value::Bool(_) => "bool".to_string(),
+        Value::None => "none".to_string(),
+    }
+}
+
+pub fn unsupported_operation(a: &Value, b: &Value, o: &str) -> String {
+    format!(
+        "Unsupported operation [{} {} {}]",
+        get_type(a),
+        o,
+        get_type(b)
+    )
+}
+
+// custom function for epxonentiating i64
+pub fn exponentiate(num: i64, exp: i64) -> Option<i64> {
+    let mut ret: i64 = num;
+
+    for _ in 1..exp {
+        let iret = ret.checked_mul(num);
+
+        match iret {
+            Option::Some(iret) => ret = iret,
+            Option::None => return Option::None,
+        }
+    }
+
+    return Option::Some(ret);
+}
 
 #[inline(always)]
 pub fn add(a: &Value, b: &Value, context: Data) -> Value {
@@ -244,6 +279,6 @@ pub fn operate(a: &Value, b: &Value, code: OpCode, context: Data) -> Value {
         OpCode::Less => less(a, b, context),
         OpCode::MoreOrEqual => more_than(a, b, context),
         OpCode::LessOrEqual => less_than(a, b, context),
-        _ => raise_internal("0012"),
+        _ => raise_internal("04"),
     }
 }
