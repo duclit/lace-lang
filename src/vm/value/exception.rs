24ab26f0085c0dd57bbc0e1b651e690b3b703025
duckie451 <96787824+duckie451@users.noreply.github.com>
2022-01-20 17:16:00 +0530

    Helper functions for raising errors

---
+++ src/vm/value/exception.rs
@@ -0,0 +1,38 @@
+use crate::error::raise_internal;
+use crate::vm::opcode::Value;
+
+pub fn get_type(value: &Value) -> String {
+    match value {
+        Value::String(_) => "string".to_string(),
+        Value::Integer(_) => "integer".to_string(),
+        Value::Float(_) => "float".to_string(),
+        Value::Array(_) => "array".to_string(),
+        Value::Bool(_) => "bool".to_string(),
+        Value::None => "none".to_string(),
+    }
+}
+
+pub fn unsupported_operation(a: &Value, b: &Value, o: &str) -> String {
+    format!(
+        "Unsupported operation [{} {} {}]",
+        get_type(a),
+        o,
+        get_type(b)
+    )
+}
+
+pub fn unsupported_conversion(a: &Value, b: u8) -> String {
+    format!(
+        "Unsupported operation [{} as {}]",
+        get_type(a),
+        match b {
+            0 => "Int",
+            1 => "Float",
+            2 => "String",
+            3 => "Array",
+            4 => "Bool",
+            5 => "None",
+            _ => raise_internal("0026"),
+        }
+    )
+}
