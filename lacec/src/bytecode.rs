101a531f0fb49ad9e811a8b050a3d298c08b1331
duclit <96787824+duclit@users.noreply.github.com>
2022-03-28 16:36:45 +0530

    Delete bytecode.rs

--- lacec/src/bytecode.rs
+++
@@ -1,7 +0,0 @@
-use crate::common::*;
-
-/* Serializes a lace program to pure bytes, compresses it and returns. */
-fn serialize(code: &(Vec<Value>, Vec<Instruction>)) -> Vec<u8> {
-    let bytes = bincode::serialize(code).unwrap();
-    bytes
-}
