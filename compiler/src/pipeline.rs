7dcf69cf30a26563d01f53cf39c9b9a22295ceaa
duclit <96787824+duclit@users.noreply.github.com>
2022-04-27 21:21:55 +0530

    Updated `lace_pipeline_init`

--- compiler/src/pipeline.rs
+++ compiler/src/pipeline.rs
@@ -1,9 +1,11 @@
+use crate::parser::Node;
 use logos::Logos;
 
-pub fn lace_pipeline_init(source: &str) {
+pub fn lace_pipeline_init(source: &str) -> Vec<Node> {
     let scanner = crate::scanner::Token::lexer(source);
     let mut parser = crate::parser::Parser::new(scanner, source.to_string());
     parser.parse();
 
-    println!("{:?}", parser.ast)
+    println!("{:?}", parser.ast);
+    parser.ast
 }
