6623c77e17d001e4da2f6da059e874b25737ebfe
duclit <96787824+duclit@users.noreply.github.com>
2022-04-30 11:28:20 +0530

    Can read files

--- shoelace/src/main.rs
+++ shoelace/src/main.rs
@@ -1,6 +1,10 @@
-use compiler;
-use std::{env, process::exit};
 use colored::*;
+use compiler;
+use hlvm;
+use std::{env, process::exit, time::Instant};
+use std::io::Read;
+use std::io::BufReader;
+use std::fs::File;
 
 fn error(err: &str) -> ! {
     println!("{}: {}", "Error".red(), err);
@@ -22,15 +26,48 @@ fn main() {
 
             let source = &args[2];
 
-            let contents = std::fs::read_to_string(source)
-                .expect("Something went wrong reading the file.");
-    
+            let contents =
+                std::fs::read_to_string(source).expect("Something went wrong reading the file.");
 
             let ast = compiler::pipeline::lace_pipeline_init(&contents);
             let mut typechecker = compiler::typecheck::Typechecker::new();
-            typechecker.check(ast);
-        },
-        "run" => todo!(),
-        _ => error("Command not found.")
+            typechecker.check(ast.clone());
+
+            let hir_instructions = compiler::codegen::compile(ast);
+            let lir_instructions = hlvm::hir::from_hir(hir_instructions);
+
+            println!("{:?}", lir_instructions);
+
+            std::fs::write("./main.o", bincode::serialize(&lir_instructions).unwrap())
+                .expect("Unable to write file");
+        }
+        "run" => {
+            if args.len() == 2 {
+                error("Expected source file.")
+            }
+
+            let source = &args[2];
+
+            let f = File::open(source)
+                .expect("Could not open file");
+            let mut reader = BufReader::new(f);
+            let mut buffer = Vec::new();
+
+            reader.read_to_end(&mut buffer)
+                .expect("Something went wrong while reading the file");
+
+            let instructions = bincode::deserialize::<Vec<hlvm::lir::HlvmInstruction>>(&buffer)
+                .expect("Unable to deserialize instructions");
+
+            let mut executor = hlvm::vm::HighLevelVirtualMachine::new(Some(1));
+
+            let start = Instant::now();
+            executor.execute(&instructions).expect("An error occured");
+            let end = start.elapsed();
+
+            println!("{:#?}", executor.call_stack);
+            println!("Execution took {:.2?}", end);
+        }
+        _ => error("Command not found."),
     }
 }
